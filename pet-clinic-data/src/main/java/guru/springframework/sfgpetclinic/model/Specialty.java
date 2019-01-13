872c10bc37977596abbc492b6a606f6130129e4a
tpekar <44039373+tpekar@users.noreply.github.com>
2019-01-13 22:42:19 +0100

    Create Vet Specialty Entity, associate with Vet. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -3,4 +3,11 @@ package guru.springframework.sfgpetclinic.model;
 public class Specialty extends BaseEntity {
     private String description;
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
