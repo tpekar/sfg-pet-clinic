4b4b9342f8a50822c6ad0d42d4eaf80bc9b84344
tpekar <44039373+tpekar@users.noreply.github.com>
2019-01-20 21:05:34 +0100

    Implemented Map Impl for Visit Service, closes # 46, #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSpecialtySDJpaService.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public VetSpecialtySDJpaService(SpecialtyRepository vetRepository) {
+        this.specialtyRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> vets = new HashSet<>();
+        specialtyRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
