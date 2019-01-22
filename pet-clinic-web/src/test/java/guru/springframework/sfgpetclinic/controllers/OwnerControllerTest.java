a71000a2e58f0c1083adb68828bf2847cc0e49f9
tpekar <44039373+tpekar@users.noreply.github.com>
2019-01-22 22:19:39 +0100

    Implemented a JUnit Test for OwnerController

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,56 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class OwnerControllerTest {
+
+    OwnerController ownerController;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        ownerController = new OwnerController(ownerService);
+    }
+
+    @Test
+    public void listOwners() {
+        assertEquals("owners/index", ownerController.listOwners(model));
+        verify(ownerService, times(1)).findAll();
+        verify(model, times(1)).addAttribute(eq("owners"), anySet());
+
+    }
+
+    @Test
+    public void findOwners() {
+        Set<Owner> owners = new HashSet<>();
+        Owner owner0 = new Owner();
+        owner0.setFirstName("Pete");
+        Owner owner1 = new Owner();
+        owner1.setFirstName("Fiona");
+        owners.add(owner0);
+        owners.add(owner1);
+
+        when(ownerService.findAll()).thenReturn(owners);
+        assertEquals(owners.size(), ownerService.findAll().size());
+    }
+}
