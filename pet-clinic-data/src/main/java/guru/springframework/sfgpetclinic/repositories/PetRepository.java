97efe0f298a32d99cc0b171f2ad62f24976cb747
tpekar <44039373+tpekar@users.noreply.github.com>
2019-02-09 18:21:42 +0100

    test commit

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -3,5 +3,5 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<Pet, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> { //test
 }
